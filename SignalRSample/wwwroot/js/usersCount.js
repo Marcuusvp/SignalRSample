5f636daee54828874a9a96a92569e59a5ed7bf6f
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-07-26 16:52:38 -0300

    configurando script de conexao ao signalR

---
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -0,0 +1,23 @@
+﻿//create connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//connect to methods that hub invokes(receive) messages from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+//invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+//start connection
+function fullfilled() {
+    console.log("Connection to User hub successfully established.");
+}
+function rejected() {
+    console.log("Connection to User hub failed.");
+}
+
+connectionUserCount.start().then(fullfilled, rejected);
