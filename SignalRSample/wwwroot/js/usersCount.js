853053f5fccb0ffcf01f19b52977dde9c74c114c
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-07-26 17:02:30 -0300

    conclusao da parte funcional do primeiro projeto com signal e resumo do funcionamento

--- SignalRSample/wwwroot/js/usersCount.js
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -1,13 +1,16 @@
 ﻿//create connection
+//Primeiro passo é a conexao via o endpoint do hub
 var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
 
 //connect to methods that hub invokes(receive) messages from hub
+//Segundo passo, conecta no método que esta no sendAsync no hub -> updateTotalViews
 connectionUserCount.on("updateTotalViews", (value) => {
     var newCountSpan = document.getElementById("totalViewsCounter");
     newCountSpan.innerText = value.toString();
 });
 
 //invoke hub methods aka send notification to hub
+//Terceiro passo, envia uma mensagem para o hub via o método criado no hub -> NewWindowLoaded
 function newWindowLoadedOnClient() {
     connectionUserCount.send("NewWindowLoaded");
 }
@@ -15,9 +18,14 @@ function newWindowLoadedOnClient() {
 //start connection
 function fullfilled() {
     console.log("Connection to User hub successfully established.");
+    newWindowLoadedOnClient();
 }
 function rejected() {
     console.log("Connection to User hub failed.");
 }
 
 connectionUserCount.start().then(fullfilled, rejected);
+
+//Resumo desse script, conecta no hub via endpoint(primeiro passo), cria uma conexao com o método do sendAsync(segundo passo),
+//usa a conexao para enviar mensagem ao hub, via o método criado no hub(terceiro passo). E por fim, inicia a conexão com o hub, se tudo der certo
+//envia uma chamada para o hub, que realiza o incremento da contagem.
