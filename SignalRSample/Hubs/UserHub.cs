5290b4af8a143b522ec0f4fe042b3ba5b7d00c47
Marcuusvp <marcusviniciuspereira1@gmail.com>
2025-07-26 12:25:19 -0300

    criando primeiro metodo no hub para notificar todos clients

--- SignalRSample/Hubs/UserHub.cs
+++ SignalRSample/Hubs/UserHub.cs
@@ -4,4 +4,15 @@ namespace SignalRSample.Hubs;
 
 public class UserHub : Hub 
 {
+    public static int TotalViews { get; set; } = 0;
+
+
+    public async Task NewWindowLoaded() 
+    {
+        TotalViews++;
+        // Notify all connected clients about the updated user count
+        // Esse metodo chamado dentro do sendAsync, deve ser criado no client side,
+        // é possível passar quantos parâmetros quiser ex: ("updateTotalViews", TotalViews, param2, param3)
+        await Clients.All.SendAsync("updateTotalViews", TotalViews);
+    }
 }
